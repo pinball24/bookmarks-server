e10b3d1395e08e1becbddc7178920e8cb3aef4e1
pinball24 <pinball101962@gmail.com>
2020-01-10 12:34:00 -0600

    finished bookmarks server

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+    id: 1,
+    title: 'Google',
+    url: 'https:www.google.com',
+    description: 'googles search engine website',
+    rating: 5
+}]
+
+module.exports = { bookmarks }
