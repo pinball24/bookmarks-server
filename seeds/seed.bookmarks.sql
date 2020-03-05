0cb425642986f16a46009cd451ea00da57a13ef6
pinball24 <pinball101962@gmail.com>
2020-03-05 11:32:30 -0600

    finished assignment for module 21.15

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks (title, url, rating, description)
+VALUES
+    ('Google', 'https://www.gooogle.com', '5', 'The best search engine'),
+    ('Bing', 'https://www.bing.com', '4', 'A great search engine'),
+    ('Thinkful', 'https://www.thinkful.com', '4', 'A place to learn programming');
--- src/app.js
+++ src/app.js
@@ -1,47 +1,29 @@
+ 
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const bookmarkRouter = require('./bookmark/bookmark-router')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
-const morganOption = (process.env.NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common'
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
 app.use(helmet())
+app.use(validateBearerToken)
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-
-  if(!authToken || authToken.split(' ')[1] !== apiToken) {
-      logger.error(`Unauthorized request to path: ${req.path}`)
-      return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  next()
-})
-
-app.use(bookmarkRouter)
+app.use(bookmarksRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: 'server error' }
-  } else {
-    console.error(error)
-    response = { error: error.message, details: error }
-  }
-  res.status(500).json(response)
-})
+app.use(errorHandler)
 
 module.exports = app
